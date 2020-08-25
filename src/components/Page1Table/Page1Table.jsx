419cd8a0290184f485f1bfd0d5d1edd1c2e5e28d
mhmufee <mahfuzul.muftee@tum.de>
2020-08-26 00:11:33 +0200

    Search bar added

--- src/components/Page1Table/Page1Table.jsx
+++ src/components/Page1Table/Page1Table.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { fade, makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -8,12 +8,18 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import DetailsTwoToneIcon from '@material-ui/icons/DetailsTwoTone';
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
 
+import Grid from "@material-ui/core/Grid";
+import DetailsTwoToneIcon from "@material-ui/icons/DetailsTwoTone";
 
 import userData from "../../res/users.json";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
   table: {
     minWidth: 650,
   },
@@ -21,7 +27,44 @@ const useStyles = makeStyles({
     overflowY: "auto",
     maxHeight: 400,
   },
-});
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.black, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(0),
+      width: "100%",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "40ch",
+    },
+  },
+}));
 
 function createData(id, name, username, website, email) {
   return { id, name, username, website, email };
@@ -42,38 +85,65 @@ export default function DenseTable() {
   }, []);
 
   return (
-    <TableContainer className={classes.container} component={Paper}>
-      <Table
-        stickyHeader
-        className={classes.table}
-        size="small"
-        aria-label="Page 1 table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">id</TableCell>
-            <TableCell align="left">Name</TableCell>
-            <TableCell align="left">User&nbsp;name</TableCell>
-            <TableCell align="left">Email</TableCell>
-            <TableCell align="left">Website</TableCell>
-            <TableCell align="left">Button</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell align="right" component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="left">{row.name}</TableCell>
-              <TableCell align="left">{row.username}</TableCell>
-              <TableCell align="left">{row.email}</TableCell>
-              <TableCell align="left">{row.website}</TableCell>
-              <TableCell align="left"><IconButton><DetailsTwoToneIcon/></IconButton></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <Paper>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <TableContainer className={classes.container} component={Paper}>
+            <Table
+              stickyHeader
+              className={classes.table}
+              size="small"
+              aria-label="Page 1 table"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right">id</TableCell>
+                  <TableCell align="left">Name</TableCell>
+                  <TableCell align="left">User&nbsp;name</TableCell>
+                  <TableCell align="left">Email</TableCell>
+                  <TableCell align="left">Website</TableCell>
+                  <TableCell align="left">Button</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.id}>
+                    <TableCell align="right" component="th" scope="row">
+                      {row.id}
+                    </TableCell>
+                    <TableCell align="left">{row.name}</TableCell>
+                    <TableCell align="left">{row.username}</TableCell>
+                    <TableCell align="left">{row.email}</TableCell>
+                    <TableCell align="left">{row.website}</TableCell>
+                    <TableCell align="left">
+                      <IconButton>
+                        <DetailsTwoToneIcon />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
