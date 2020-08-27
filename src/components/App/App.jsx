721018a7813c8f30d04ba1910fbd9aa729fed27d
mhmufee <mahfuzul.muftee@tum.de>
2020-08-27 14:12:03 +0200

    TabPanel modified

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,5 @@
 import React, {useEffect} from "react";
-import PropTypes from "prop-types";
 import Drawer from "@material-ui/core/Drawer";
-import Box from "@material-ui/core/Box";
 import AppBar from "@material-ui/core/AppBar";
 import { Tab, Tabs } from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -14,30 +12,9 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 
 import Page1 from "pages/Page1";
 import Page2 from "pages/Page2";
+import TabPanel from 'components/TabPanel';
 
 const drawerWidth = 170;
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={1}>{children}</Box>}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
 
 function a11yProps(index) {
   return {
