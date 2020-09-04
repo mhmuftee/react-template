f551e1183a8e0fa3571ca2d43703c3a660c42ea1
mhmufee <mahfuzul.muftee@tum.de>
2020-09-04 10:14:40 +0200

    Action column moved to end

---
+++ src/components/TestPageTable/TestPageTable.jsx
@@ -0,0 +1,147 @@
+import React, { forwardRef, useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import MaterialTable from "material-table";
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+
+import { connect } from "react-redux";
+import {
+  fetchDroovData,
+  putDroovData,
+  deleteDroovData,
+} from "../../actions/postAction";
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    height: "100vh",
+    width: "100vw",
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(0),
+    overflow: 'auto'
+  },
+}));
+
+function TestPageTable(props) {
+  const classes = useStyles();
+
+  const [state, setState] = useState({
+    columns: [
+      { title: "id", field: "id", editable: "never" },
+      { title: "name", field: "name" },
+      { title: "username", field: "username" },
+      { title: "email", field: "email" },
+      { title: "website", field: "website" },
+    ],
+    data: [],
+  });
+
+  const fetch = props.fetchDroovData;
+
+  useEffect(() => {
+    fetch();
+  }, [fetch]);
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <main className={classes.content}>
+        <MaterialTable
+          title="Editable Table"
+          icons={tableIcons}
+          columns={state.columns}
+          data={props.data}
+          actions={[
+            {
+              icon: 'save',
+              tooltip: 'Save User',
+              onClick: (event, rowData) => {props.changetab(1, rowData.name)}
+            }]}
+          options={{
+            actionsColumnIndex: -1
+          }}
+          editable={{
+            onRowAdd: (newData) =>
+              new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve();
+                  props.putDroovData(newData);
+                }, 600);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve();
+                  if (oldData) {
+                    setState((prevState) => {
+                      const data = [...prevState.data];
+                      data[data.indexOf(oldData)] = newData;
+                      return { ...prevState, data };
+                    });
+                  }
+                }, 600);
+              }),
+            onRowDelete: (oldData) =>
+              new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve();
+                  props.deleteDroovData(oldData.id);
+                }, 600);
+              }),
+          }}
+        />
+      </main>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  data: state.posts.droovItems,
+});
+
+export default connect(mapStateToProps, {
+  fetchDroovData,
+  putDroovData,
+  deleteDroovData,
+})(TestPageTable);
--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,138 +1,114 @@
-import React, { forwardRef, useEffect, useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Collapse from "@material-ui/core/Collapse";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import MaterialTable from "material-table";
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import Edit from "@material-ui/icons/Edit";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
+import List from "@material-ui/core/List";
+import { Tab, Tabs } from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 
-import { connect } from "react-redux";
-import {
-  fetchDroovData,
-  putDroovData,
-  deleteDroovData,
-} from "../../actions/postAction";
+import TestPageTable from "components/TestPageTable";
+import Page2 from "pages/Page2";
+import TabPanel from "components/TabPanel";
 
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref} />
-  )),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => (
-    <ChevronLeft {...props} ref={ref} />
-  )),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-};
+const drawerWidth = 120;
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     height: "100vh",
-    width: "100vw",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    marginLeft: 120,
+  },
+  drawerContainer: {
+    overflow: "auto",
   },
   toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     padding: theme.spacing(1),
+    backgroundColor: "white",
   },
 }));
 
-function TestPage(props) {
+export default function TestPage(props) {
   const classes = useStyles();
 
-  const [state, setState] = useState({
-    columns: [
-      { title: "id", field: "id", editable: "never" },
-      { title: "name", field: "name" },
-      { title: "username", field: "username" },
-      { title: "email", field: "email" },
-      { title: "website", field: "website" },
-    ],
-    data: [],
-  });
+  const [open, setOpen] = React.useState(true);
+  const [tabindex, setTabIndex] = React.useState(0);
+  const [username, setUsername] = React.useState("Not found");
 
-  const fetch = props.fetchDroovData;
+  const clickTab = (event, index) => {
+    setTabIndex(index);
+  };
 
-  useEffect(() => {
-    fetch();
-  }, [fetch]);
+  const clickMenu = () => {
+    setOpen(!open);
+  };
+
+  const changeTab = (tabIndex, username = "Not found") => {
+    setTabIndex(tabIndex);
+    setUsername(username);
+  };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <div className={classes.drawerContainer}>
+          <List>
+            <ListItem button onClick={clickMenu}>
+              <ListItemText primary="Menu" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={tabindex}
+                onChange={clickTab}
+                aria-label="Vertical tabs"
+                className={classes.tabs}
+              >
+                <Tab label="Page 1" {...a11yProps(0)} />
+                <Tab label="Page 2" {...a11yProps(1)} />
+              </Tabs>
+            </Collapse>
+          </List>
+        </div>
+      </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <MaterialTable
-          title="Editable Table"
-          icons={tableIcons}
-          columns={state.columns}
-          data={props.data}
-          editable={{
-            onRowAdd: (newData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  props.putDroovData(newData);
-                }, 600);
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  if (oldData) {
-                    setState((prevState) => {
-                      const data = [...prevState.data];
-                      data[data.indexOf(oldData)] = newData;
-                      return { ...prevState, data };
-                    });
-                  }
-                }, 600);
-              }),
-            onRowDelete: (oldData) =>
-              new Promise((resolve) => {
-                setTimeout(() => {
-                  resolve();
-                  props.deleteDroovData(oldData.id);
-                }, 600);
-              }),
-          }}
-        />
+        <TabPanel value={tabindex} index={0}>
+          <TestPageTable changetab={changeTab} />
+        </TabPanel>
+        <TabPanel value={tabindex} index={1}>
+          <Page2 changetab={changeTab} username={username} />
+        </TabPanel>
       </main>
     </div>
   );
 }
-
-const mapStateToProps = (state) => ({
-  data: state.posts.droovItems,
-});
-
-export default connect(mapStateToProps, {
-  fetchDroovData,
-  putDroovData,
-  deleteDroovData,
-})(TestPage);
